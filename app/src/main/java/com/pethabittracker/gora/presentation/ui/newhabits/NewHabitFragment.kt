2559769d129a7734f1788137dc067273ce097dbe
Arseni Hlaz <mafusaik@gmail.com>
2023-01-26 22:00:11 +0300

    week button examples

--- app/src/main/java/com/pethabittracker/gora/presentation/ui/newhabits/NewHabitFragment.kt
+++ app/src/main/java/com/pethabittracker/gora/presentation/ui/newhabits/NewHabitFragment.kt
@@ -1,13 +1,17 @@
 package com.pethabittracker.gora.presentation.ui.newhabits
 
+import android.graphics.Color
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
+import com.pethabittracker.gora.R
 import com.pethabittracker.gora.databinding.FragmentNewHabitBinding
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -32,7 +36,7 @@ class NewHabitFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
-            toolbarDetail.setNavigationOnClickListener{
+            toolbarDetail.setNavigationOnClickListener {
                 findNavController().navigateUp()
             }
 
@@ -48,6 +52,41 @@ class NewHabitFragment : Fragment() {
 
                 findNavController().navigateUp()
             }
+            var tuesdayFlag = false
+            var mondayFlag = false
+
+            tuesday.setOnClickListener {
+                if (!tuesdayFlag) {
+                    it.setBackgroundColor(getColor(requireContext(), R.color.button_pressed))
+                    tuesdayFlag = true
+                } else {
+                    it.setBackgroundColor(getColor(requireContext(), R.color.white))
+                    tuesdayFlag = false
+                }
+            }
+            monday.setOnClickListener {
+                if (!mondayFlag) {
+                    it.setBackgroundColor(getColor(requireContext(), R.color.button_pressed))
+                    mondayFlag = true
+                } else {
+                    it.setBackgroundColor(getColor(requireContext(), R.color.white))
+                    mondayFlag = false
+                }
+            }
+
+            wednesday2.setOnClickListener {
+                if (it.isPressed){
+                    it.setBackgroundResource(R.color.button_pressed)
+                } else{
+                    it.setBackgroundResource(R.color.white)
+                }
+            }
+//
+//            tuesday2.setOnClickListener {
+//                if (it.isPressed){
+//                    it.setBackgroundResource(R.color.button_pressed)
+//                }
+//            }
         }
     }
 
